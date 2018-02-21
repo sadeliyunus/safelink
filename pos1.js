5d30399d85b8aaa00b545a4f729124f64acb542e
sadeliyunus <36619933+sadeliyunus@users.noreply.github.com>
2018-02-21 23:26:10 +0700

    Update pos1.js

--- pos1.js
+++ pos1.js
@@ -18,5 +18,5 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href=" http://gretonglovers.blogspot.com/2018/02/whats-origin-of-valentines-day.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href=" https://gretonglovers.blogspot.com/2018/02/whats-origin-of-valentines-day.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
